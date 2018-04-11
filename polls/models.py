67e958c0ff36d133eba856fd0699025736456bea
jixilin <1778202464@qq.com>
2018-04-11 15:44:45 +0800

    django入门的程序就这么写完了，后面还有一个将这个应用修改为一个可以复用的app的教程。看看再说了

--- polls/models.py
+++ polls/models.py
@@ -15,11 +15,14 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-    # 打印当前的时间
-
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 
 class Choice(models.Model):
--- pythonWeb/settings.py
+++ pythonWeb/settings.py
@@ -124,3 +124,19 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+# DIRS is a list of filesystem directories to check when loading Django templates; it’s a search path.
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
